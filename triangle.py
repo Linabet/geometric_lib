457ca56e8e25fa17e16ec83794a543d966c89dd7
waseelahmed <46781397+waseelahmed@users.noreply.github.com>
2023-10-04 20:13:40 +0100

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,4 +1,9 @@
+
+# Function to return area e.g) var_area = area(5,5)
 def area(a, h): 
+        ''' 
+        This methd returns the area of a rectangle
+        '''
         return a * h / 2
 
     def perimeter(a, b, c): 
